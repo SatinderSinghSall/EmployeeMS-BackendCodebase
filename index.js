1e5ae82710ae46ddf3b6c9a04b24439f568af14b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-10 01:10:39 +0530

    Fixed the deployment error.

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
